514bf2b58deb000c43cdc591d510dd4f1f80e783
William Grant-Buckley <william@city.uk.com>
2016-02-19 16:44:36 +0000

    Allow basic pagination

--- src/Tasks/BuildSiteTask.php
+++ src/Tasks/BuildSiteTask.php
@@ -30,23 +30,72 @@ class BuildSiteTask implements TaskInterface
     public function run()
     {
         $config = $this->container['config'];
-        $fs = $this->container['fs'];
-        $twig = $this->container['twig'];
 
         foreach ($this->content as $page) {
-            $path = rtrim($this->target, '/') . '/' . $page->get('uri');
             $twigData = array_merge([
                 'categories' => $this->content->pluck('category')->unique()->filter(),
                 'config' => $config,
                 'content' => $this->content,
                 'tags' => $this->content->pluck('tags')->flatten()->values()->unique()->filter(),
-            ], [
-                'page' => $page,
-            ]);
+            ], compact('page'));
 
-            $fs->dumpFile($path, $twig->render($page->get('layout'), $twigData));
+            if ($page->has('pagination')) {
+                $this->handlePagination($page, $twigData);
+                continue;
+            }
+
+            $this->writeFile($page, $twigData);
         }
 
         return Result::success($this, 'Generated static HTML');
+    }
+
+    private function writeFile($page, $data)
+    {
+        $fs = $this->container['fs'];
+        $twig = $this->container['twig'];
+
+        $fs->dumpFile(
+            rtrim($this->target, '/') . $page->get('uri'),
+            $twig->render($page->get('layout'), $data)
+        );
+    }
+
+    private function handlePagination($page, $twigData)
+    {
+        $pagination = collect($page->get('pagination'));
+
+        $type = $pagination->get('use', true);
+        $size = $pagination->get('size', null);
+
+        $scopedContent = $this->content->where('layout', $type);
+        $contentCount = $scopedContent->count();
+        $pageCount = ceil($contentCount / ($size ? $size : 1));
+
+        $getPagedPath = function ($pageNumber) use ($page) {
+            $uri = $page->get('uri');
+            return $pageNumber > 1 ? str_replace('.html', '-' . $pageNumber . '.html', $uri) : $uri;
+        };
+
+        for ($pageNumber = 1; $pageNumber <= $pageCount; $pageNumber++ ) {
+            $page->put('uri', $getPagedPath($pageNumber));
+
+            $pagination->put('items', $scopedContent->forPage($pageNumber, $size));
+            $pagination->put('current', $pageNumber);
+            $pagination->put('current_uri', $getPagedPath($pageNumber));
+            $pagination->put('next', $pageNumber < $pageCount ? $pageNumber + 1 : null);
+            $pagination->put('next_uri', $pageNumber < $pageCount ? $getPagedPath($pageNumber + 1) : null);
+            $pagination->put('previous', $pageNumber > 1 ? $pageNumber - 1 : null);
+            $pagination->put('previous_uri', $pageNumber > 1 ? $getPagedPath($pageNumber - 1) : null);
+            $pagination->put('first', 1);
+            $pagination->put('first_uri', $getPagedPath(1));
+            $pagination->put('last', $pageCount);
+            $pagination->put('last_uri', $getPagedPath($pageCount));
+            $pagination->put('total', $contentCount);
+
+            $twigData['pagination'] = $pagination;
+
+            $this->writeFile($page, $twigData);
+        }
     }
 }
